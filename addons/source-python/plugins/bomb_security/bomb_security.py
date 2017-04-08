7857f8e45a3e62b11a4b6790fe47734afc0d2c2f
Stephen Toon <satoon101@gmail.com>
2017-04-08 00:52:06 -0400

    Changed to use a class to store the C4 bumper's team.

--- addons/source-python/plugins/bomb_security/bomb_security.py
+++ addons/source-python/plugins/bomb_security/bomb_security.py
@@ -12,9 +12,29 @@ from memory import make_object
 
 
 # =============================================================================
-# >> GLOBAL VARIABLES
+# >> CLASSES
 # =============================================================================
-_bump_player = None
+class _BumpManager(object):
+    bump_player = None
+
+    def pre_bump_weapon(self, stack_data):
+        """Switch the player's team if they are a CT picking up the bomb."""
+        if make_object(Entity, stack_data[1]).classname != 'weapon_c4':
+            return
+        self.bump_player = make_object(Entity, stack_data[0])
+        if self.bump_player.team == 3:
+            self.bump_player.team = 2
+        else:
+            self.bump_player = None
+
+    def post_bump_weapon(self):
+        """Switch the player back to CT if they just picked up the bomb."""
+        if self.bump_player is None:
+            return
+        self.bump_player.team = 3
+        self.bump_player = None
+
+_bump_manager = _BumpManager()
 
 
 # =============================================================================
@@ -23,23 +43,10 @@ _bump_player = None
 @EntityPreHook(EntityCondition.is_human_player, 'bump_weapon')
 @EntityPreHook(EntityCondition.is_bot_player, 'bump_weapon')
 def _pre_bump_weapon(stack_data):
-    """Switch the player's team if they are a CT picking up the bomb."""
-    global _bump_player
-    if make_object(Entity, stack_data[1]).classname != 'weapon_c4':
-        return
-    _bump_player = make_object(Entity, stack_data[0])
-    if _bump_player.team == 3:
-        _bump_player.team = 2
-    else:
-        _bump_player = None
+    _bump_manager.pre_bump_weapon(stack_data)
 
 
 @EntityPostHook(EntityCondition.is_human_player, 'bump_weapon')
 @EntityPostHook(EntityCondition.is_bot_player, 'bump_weapon')
 def _post_bump_weapon(stack_data, return_value):
-    """Switch the player's team back to CT if they just picked up the bomb."""
-    global _bump_player
-    if _bump_player is None:
-        return
-    _bump_player.team = 3
-    _bump_player = None
+    _bump_manager.post_bump_weapon()
