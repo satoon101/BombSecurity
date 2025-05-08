1b1ccefe1d37b45ff6447cef31b4718c1524dfab
satoon101 <satoon101@gmail.com>
2025-05-08 18:38:06 -0400

    Updates for Ruff.

--- addons/source-python/plugins/bomb_security/bomb_security.py
+++ addons/source-python/plugins/bomb_security/bomb_security.py
@@ -9,6 +9,7 @@
 from entities.entity import Entity
 from entities.hooks import EntityCondition, EntityPostHook, EntityPreHook
 from memory import make_object
+from players.teams import teams_by_name
 
 
 # =============================================================================
@@ -19,11 +20,11 @@ class _BumpManager:
 
     def pre_bump_weapon(self, stack_data):
         """Switch the player's team if they are a CT picking up the bomb."""
-        if make_object(Entity, stack_data[1]).classname != 'weapon_c4':
+        if make_object(Entity, stack_data[1]).classname != "weapon_c4":
             return
         self.bump_player = make_object(Entity, stack_data[0])
-        if self.bump_player.team == 3:
-            self.bump_player.team_index = 2
+        if self.bump_player.team == teams_by_name["ct"]:
+            self.bump_player.team_index = teams_by_name["t"]
         else:
             self.bump_player = None
 
@@ -31,7 +32,7 @@ class _BumpManager:
         """Switch the player back to CT if they just picked up the bomb."""
         if self.bump_player is None:
             return
-        self.bump_player.team_index = 3
+        self.bump_player.team_index = teams_by_name["ct"]
         self.bump_player = None
 
 
@@ -41,13 +42,13 @@ _bump_manager = _BumpManager()
 # =============================================================================
 # >> ENTITY HOOKS
 # =============================================================================
-@EntityPreHook(EntityCondition.is_human_player, 'bump_weapon')
-@EntityPreHook(EntityCondition.is_bot_player, 'bump_weapon')
+@EntityPreHook(EntityCondition.is_human_player, "bump_weapon")
+@EntityPreHook(EntityCondition.is_bot_player, "bump_weapon")
 def _pre_bump_weapon(stack_data):
     _bump_manager.pre_bump_weapon(stack_data)
 
 
-@EntityPostHook(EntityCondition.is_human_player, 'bump_weapon')
-@EntityPostHook(EntityCondition.is_bot_player, 'bump_weapon')
+@EntityPostHook(EntityCondition.is_human_player, "bump_weapon")
+@EntityPostHook(EntityCondition.is_bot_player, "bump_weapon")
 def _post_bump_weapon(stack_data, return_value):
     _bump_manager.post_bump_weapon()
